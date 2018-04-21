7642eeb8251d436a68ef6375a41a8dc67bac9636
mlehotsky13 <mlehotsky13@gmail.com>
2018-04-21 15:01:10 +0200

    Update

--- testing.R
+++ testing.R
@@ -81,14 +81,10 @@ vc_p <- tm_map(vc_p, stripWhitespace)
 # remove unused structures
 remove(allDocs, cgi, docsOfFile, files, i, ls, topic, cgiDt, lsDt, topicDt, vc, vs)
 
+wanted_topics <- list("earn", "acq", "money-fx", "crude", "grain")
 wanted <- tm_filter(vc_p, FUN = function(x){length(meta(x)[["topics_cat"]]) == 1})
-wanted <- tm_filter(wanted, FUN = function(x){
-  "crude" == meta(x)[["topics_cat"]] | "money-fx" == meta(x)[["topics_cat"]] | "trade" == meta(x)[["topics_cat"]]
-})
+wanted <- tm_filter(wanted, FUN = function(x){any(meta(x)[["topics_cat"]] %in% wanted_topics)})
 wanted <- tm_filter(wanted, FUN = function(x){meta(x)[["lewissplit"]] %in% c("TRAIN", "TEST")})
-
-#wanted_train <- tm_filter(wanted, FUN = function(x){meta(x)[["lewissplit"]] == "TRAIN"})
-#wanted_test <- tm_filter(wanted, FUN = function(x){meta(x)[["lewissplit"]] == "TEST"})
 
 dataframe <- data.frame(text=unlist(sapply(wanted, `[`, "content")),
                         topic=unlist(sapply(wanted, meta, "topics_cat")),
@@ -100,8 +96,8 @@ corpus <- Corpus(sourceData)
 dtm <- DocumentTermMatrix(corpus)
 weightedDtm <- weightTfIdf(dtm)
 
-dtm <- removeSparseTerms(dtm, 0.9)
-weightedDtm <- removeSparseTerms(weightedDtm, 0.9)
+dtm <- removeSparseTerms(dtm, 0.98)
+weightedDtm <- removeSparseTerms(weightedDtm, 0.98)
 
 dataframeDtm <- data.frame(as.matrix(dtm))
 dataframeWeightedDtm <- data.frame(as.matrix(weightedDtm))
@@ -131,91 +127,5 @@ svm.tfidf.radial.predict <- predict(svm.tfidf.radial,newdata = dataframeWeighted
 
 svm.tfidf.linear
 svm.tfidf.radial
-
-#dataframe2 <- data.frame(as.matrix(weightedDtm))
-#dataframe2$topic <- dataframe$topic
-
-# get subset corpus of train documents
-vc_p_train <- tm_filter(vc_p, FUN = function(x){meta(x)[["lewissplit"]] == "TRAIN"})
-vc_p_test <- tm_filter(vc_p, FUN = function(x){meta(x)[["lewissplit"]] == "TEST"})
-
-vc_p_train_1 <- tm_filter(vc_p_train, FUN = function(x){length(meta(x)[["topics_cat"]]) == 1})
-vc_p_train_1_non_empty <- tm_filter(vc_p_train_1, FUN = function(x){length(x$content) != 0})
-
-vc_p_train_3 <- tm_filter(vc_p_train_1_non_empty, FUN = function(x){
-  length(meta(x)[["topics_cat"]]) == 1 & "crude" %in% meta(x)[["topics_cat"]]  | "money-fx" == meta(x)[["topics_cat"]] | "trade" == meta(x)[["topics_cat"]]
-})
-
-dataframe <- data.frame(text=unlist(sapply(vc_p_train_3[1:100], `[`, "content")),
-                        topic=unlist(sapply(vc_p_train_3[1:100], meta, "topics_cat")),
-                        stringsAsFactors=F)
-
-
-# vytvorenie corpusu obsahujuceho dokumenty s topicom "earn" => pozitivne priklad
-# a dokumentov 
-#vc_p_train_earn <- tm_filter(vc_p_train, FUN = function(x){
-#  "earn" %in% meta(x)[["topics_cat"]] | length(meta(x)[["topics_cat"]]) == 0
-#});
-
-vc_p_one <- tm_filter(vc_p_train, FUN = function(x){length(meta(x)[["topics_cat"]]) == 1})
-
-dataframe <- data.frame(text=unlist(sapply(vc_p_one[1:10], `[`, "content")),
-                        #train_test=sapply(vc_p_train[1:10], meta, "lewissplit"),
-                        topics=unlist(sapply(vc_p_one[1:10], function(x){
-                          list(meta(x)[["topics_cat"]])
-                        })),
-                        stringsAsFactors=F)
-
-dataframe <- data.frame(topics_cat=lapply(vc_p_train[1:10], FUN = function(x){
-  if (length(meta(x)[["topics_cat"]]) == 0){
-    return("NA");
-  } else {
-    return(meta(x)[["topics_cat"]]);
-  }
-}))
-
-#text=lapply(unlist(lapply(sapply(corpus, '[', "content"),paste,collapse="\n")),
-#            stringsAsFactors=FALSE))
-
-# create Term Document Matrices
-tdm_train <- TermDocumentMatrix(vc_p_train)
-tdm_tfidf_train <- weightTfIdf(tdm_train)
-tdm_test <- TermDocumentMatrix(vc_p_test)
-tdm_tfidf_test <- weightTfIdf(tdm_test)
-
-# create data frames
-df_train <- as.data.frame(inspect(tdm_train))
-df_tfidf_train <- as.data.frame(inspect(tdm_tfidf_train))
-df_test <- as.data.frame(inspect(tdm_test))
-df_tfidf_test <- as.data.frame(inspect(tdm_tfidf_test))
-
-# creating Term Document Matrix
-#tdm <- TermDocumentMatrix(vc_p1000)[, 1:10]
-#dim(tdm)
-
-# creating matrix
-#tdmMatrix <- as.matrix(tdm)
-#tdmMatrix <- cbind.data.frame(tdmMatrix, train_test=rep("train", nrow(tdmMatrix)))
-#freq <- sort(rowSums(tdmMatrix), decreasing=TRUE)
-#wf <- data.frame(word=names(freq), freq=freq)
-
-# construct graph of term frequencies
-#p <- ggplot(subset(wf, freq>100), aes(x = reorder(word, -freq), y = freq)) +
-#  geom_bar(stat = "identity") + 
-#  theme(axis.text.x=element_text(angle=45, hjust=1))
-#p
-
-#findFreqTerms(tdm, 50)
-#findAssocs(tdm, "said", 0.7)
-#tdm_p <- removeSparseTerms(tdm, 0.8)
-#inspect(tdm_p)
-
-#library(reshape2)
-#TDM.dense = melt(tdm.dense, value.name = "count")
-#head(tdm.dense)
-
-#weightedtdm <- weightTfIdf(tdm)
-
-#as.matrix(weightedtdm)[10:20,200:210]
 
 #SAMOSTATNY MODEL pre kazdy TOPIC, dokument ho ma alebo nema
